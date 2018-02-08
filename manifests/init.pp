fafca1a3c1e0a9147ba7d4518ce1b4a1bd730b3a
Chris Smith <huricane.smithy@gmail.com>
2018-02-07 19:42:34 -0500

    More command differences

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,23 +52,6 @@ class composer (
   validate_string($group)
   validate_bool($build_deps)
 
-  case $::kernel {
-    'Darwin': {
-      $download_command = '/usr/bin/curl --insecure'
-      $download_require = undef
-      $test_exec = '/bin/test'
-    }
-    default: {
-      $download_command = '/usr/bin/wget --no-check-certificate'
-      $download_require = Package['wget']
-      $test_exec = '/usr/bin/test'
-      
-      if $build_deps {
-        ensure_packages(['wget'])
-      }
-    }
-  }
-
   include composer::params
 
   $target = $version ? {
@@ -82,9 +65,26 @@ class composer (
     undef   => "${test_exec} -f ${composer_full_path}",
     default => "${test_exec} -f ${composer_full_path} && ${composer_full_path} -V |grep -q ${version}"
   }
+  
+  case $::kernel {
+    'Darwin': {
+      $download_command = '/usr/bin/curl --insecure -o ${composer_full_path} ${target}'
+      $download_require = undef
+      $test_exec = '/bin/test'
+    }
+    default: {
+      $download_command = '/usr/bin/wget --no-check-certificate -O ${composer_full_path} ${target}'
+      $download_require = Package['wget']
+      $test_exec = '/usr/bin/test'
+      
+      if $build_deps {
+        ensure_packages(['wget'])
+      }
+    }
+  }
 
   exec { 'composer-install':
-    command     => "${download_command} -O ${composer_full_path} ${target}",
+    command     => "${download_command}",
     environment => [ "COMPOSER_HOME=${target_dir}" ],
     user        => $user,
     unless      => $unless,
